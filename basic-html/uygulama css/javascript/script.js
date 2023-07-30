7ea62f119f882991aa9b4c4bd160e06736a832a5
oguzhan <oguzhanselamoglu@outlook.com>
2023-07-30 18:21:07 +0300

    Create script.js

---
+++ basic-html/uygulama css/javascript/script.js
@@ -0,0 +1,24 @@
+
+let ogrenci1 = 'seda';
+let ogrenci1Year = 2012;
+let ogrenci1Not1 = 70;
+let ogrenci1Not2 = 70;
+let ogrenci1Not3 = 80;
+let ogrenci1Yas = 2023 - ogrenci1Year;
+let notOrtalama1 = (ogrenci1Not1 + ogrenci1Not2 + ogrenci1Not3) / 3;
+let ogrenci1Basari = (notOrtalama1 >= 50 );
+
+let ogrenci2 = 'oguzhan';
+let ogrenci2Year = 2010;
+let ogrenci2Not1 = 40;
+let ogrenci2Not2 = 40;
+let ogrenci2Not3 = 50;
+let ogrenci2Yas = 2023 - ogrenci2Year;
+let notOrtalama2 = (ogrenci2Not1 + ogrenci2Not2 + ogrenci2Not3) / 3;
+let ogrenci2Basari = (notOrtalama2 >= 50 );
+
+
+console.log(ogrenci1Basari)
+console.log(ogrenci2Basari)
+
+
